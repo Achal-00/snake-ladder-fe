a947dd33f1f59dc27d70247e72e4f4c0034963d6
Achal <achaldev3@gmail.com>
2023-10-06 13:20:56 +0530

    changed image path

--- app/components/ControlSection/PawnDetails.jsx
+++ app/components/ControlSection/PawnDetails.jsx
@@ -2,11 +2,11 @@ export default function PawnDetails() {
   return (
     <div className="flex flex-col gap-4">
       <div className="flex items-center gap-4">
-        <img src="/pawn-red.svg" alt="" className="w-7" />
+        <img src="pawn-red.svg" alt="" className="w-7" />
         <p className="text-sm font-medium text-purple-500">Your Pawn</p>
       </div>
       <div className="flex items-center gap-4">
-        <img src="/pawn-yellow.svg" alt="" className="w-7" />
+        <img src="pawn-yellow.svg" alt="" className="w-7" />
         <p className="text-sm font-medium text-purple-500">Opponent's Pawn</p>
       </div>
     </div>
